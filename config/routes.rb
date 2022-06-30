4eb5a3d1f9d0d0915e02a04d2cc93de0552ea771
Mark Goho <markgoho@gmail.com>
2022-06-30 21:49:02 +0000

    rails 7 outputting basic html from db

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  get '/articles', to: 'articles#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
